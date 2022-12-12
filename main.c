be4569b145d3547deed0ad5f8cf3c08941f92c7f
Ethan1709 <benyayerethan@gmail.com>
2022-12-12 08:34:05 -0800

    test 4

--- main.c
+++ main.c
@@ -7,10 +7,13 @@ int main (int argc, char **argv)
 	(void)argv;
 
 	int i = 0;
+	int x = 0;
+
+	x = atoi(argv[1]);
 
 	if (_isdigit(i) == 0)
 	{
-		fprintf(stderr, "L8: push %d: push integer\n", i);
+		fprintf(stderr, "L%x: push %d: push integer\n", x, i);
 		exit(EXIT_FAILURE);
 	}
 	return (i);
