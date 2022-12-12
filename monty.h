d5a6bb340af09966ee9d4d190230e7233955df7c
Ethan1709 <benyayerethan@gmail.com>
2022-12-12 03:14:04 -0800

    add README.md and header files

---
+++ monty.h
@@ -0,0 +1,19 @@
+#define _LIFO_FIFO
+#ifndef _LIFO_FIFO
+
+#include <sdlib.h>
+#include <stdio.h>
+
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+#endif
