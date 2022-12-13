6d6eeb990241ac0f3930f152f7493b745d105903
bapt2 <5711@holbertonstudents.com>
2022-12-13 06:16:29 -0800

    add op code: pall, pint, nop

--- opcode.c
+++ opcode.c
@@ -1,6 +1,12 @@
 #include "monty.h"
 
-int main(unsigned int line_nuber, stack_t **stack)
+/**
+ * _push - push an element to the stack
+ * @line_number: the line where the error is
+ * @stack: the stack
+ * Return: void
+*/
+void push(unsigned int line_number, stack_t **stack)
 {
 	int i = 0;
 	char *token;
@@ -18,6 +24,67 @@ int main(unsigned int line_nuber, stack_t **stack)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
+
+}
+
+/**
+ * _pall - print all the value of the stack, starting from the top
+ * @ln: line number
+ * @stack: the stack
+ * Return: void
+*/
+void _pall(unsigned int ln, stack_t **stack)
+{
+	stack_t *pos = NULL;
+	(void)ln;
+
+	pos = *stack;
+	while (pos != NULL)
+	{
+		dprintf(stdout, "%d\n", pos->n);
+		pos = pos->next;
+	}
+}
+
+/**
+ * _pint - print the value at the top of the stack
+ * @line_number: the line where the error is
+ * @stack: the stack
+ * Return: void
+*/
+void _pint(unsigned int line_number, stack_t **stack)
+{
+	stack_t *pos = NULL;
+
+	pos = *stack;
+	if (pos != 0)
+		dprintf(stdout, "%d\n", pos->n);
+	else
+	{
+		fprintf(stderr, "L%u: con't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * _pop - remove the top element of the stack
+ * @line_number: the line where the error is
+ * @stack: the stack
+ * Return: void
+*/
+void _pop(unsigned int line_number, stack_t **stack)
+{
 	
-	return (0);
+
+}
+
+/**
+ * _nop - do nothing
+ * Return: void
+*/
+
+void _nop(unsigned int line_number, stack_t **stack)
+{
+	(void)line_number;
+	(void)stack;
 }
